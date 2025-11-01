dce511b0f69ce5eebac2752621d5a1718a470bcf
mahmoodZargari <nmashoomy11@gmail.com>
2025-11-01 13:16:40 +0330

    Delete utils directory

--- utils/index.js
+++
@@ -1,8 +0,0 @@
-const createPaginationData = (page, limit, totalCount, resourceName) => ({
-    page: parseInt(page),
-    limit: parseInt(limit),
-    totalPage: Math.ceil(totalCount / limit),
-    [`total${resourceName}`]: totalCount,
-});
-
-module.exports = {createPaginationData};
